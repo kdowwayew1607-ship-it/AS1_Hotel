eb705f05df5468cb09bea96a56235407219f1ee5
kdowwayew1607-ship-it <kdowwayew1607@gmail.com>
2025-10-01 21:01:35 +0500

    1.0.2

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -20,11 +20,12 @@ class BookingFactory extends Factory
     {
         $roomId = Room::inRandomOrder()->first();
         $guestId = Guest::inRandomOrder()->first();
+        $check_in = fake()->dateTimeBetween('-10 days', '-1 days');
         return [
             'room_id' => $roomId ? $roomId->id :1,
             'guest_id' => $guestId ? $guestId->id :1,
-            'check_in_at' => fake()->dateTimeThisYear(),
-            'check_out_at' => fake()->dateTimeThisYear(),
+            'check_in_at' => $check_in,
+            'check_out_at' => fake()->dateTimeBetween($check_in, 'now'),
         ];
     }
 }
